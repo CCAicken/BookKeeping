1c1582f3ec3c8bdd3cc819b6f48e4a97aa076414
jock <jock@DESKTOP-I2S4DFK>
2019-09-07 15:36:11 +0800

    usercontroller 注册Spring

--- BookKeep/src/controller/service/UserController.java
+++ BookKeep/src/controller/service/UserController.java
@@ -6,6 +6,7 @@ import java.io.Writer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +17,7 @@ import business.impl.UserDaoImpl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
+@Controller
 @RequestMapping(value = "user")
 public class UserController {
 	@RequestMapping(value = "getuserid")
